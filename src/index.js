7b38afd3a02a0d6ea4c2b927f782d0406c545c69
prescott breeden <pb@prescotts-iMac.local>
2018-05-17 20:25:34 -0700

    project init
    first react project - making a SPA with youtube API integration

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const youTubeAPI_key = 'AIzaSyAx1AXPFztabysf6PaKemcRCtYuYS1cRFs'
+
+
+const App = () => {
+    return <div>Hi!</div>;
+}
+
+ReactDOM.render(<App />, document.querySelector('.container'));
+
